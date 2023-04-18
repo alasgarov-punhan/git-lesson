24bdb7072baba054616332ad9bbc87bb3175d5e5
punhanalasgarov <punhan.alasgarov@sinam.net>
2023-04-18 19:18:09 +0400

    create model package and User class added constructor

---
+++ src/main/java/az/alasgarov/model/User.java
@@ -0,0 +1,22 @@
+package az.alasgarov.model;
+
+public class User {
+    private String name;
+    private String age;
+
+    public User() {
+    }
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
