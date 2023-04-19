2068a6f368d8affce33be1ad5add873d42e58747
punhanalasgarov <punhan.alasgarov@sinam.net>
2023-04-19 17:17:32 +0400

    UserDto class added

---
+++ src/main/java/az/alasgarov/dto/UserDto.java
@@ -0,0 +1,11 @@
+package az.alasgarov.dto;
+
+public class UserDto {
+    private String name;
+    private String age;
+
+    public UserDto(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+}
