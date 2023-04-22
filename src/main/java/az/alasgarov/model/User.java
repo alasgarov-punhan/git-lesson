70a07da212468e89d40d3715b366c9bbeb44b1fe
punhanalasgarov <punhan.alasgarov@sinam.net>
2023-04-22 09:52:26 +0400

    added phoneNumber User class

--- src/main/java/az/alasgarov/model/User.java
+++ src/main/java/az/alasgarov/model/User.java
@@ -3,13 +3,15 @@ package az.alasgarov.model;
 public class User {
     private String name;
     private String age;
+    private String phoneNumber;
 
     public User() {
     }
 
-    public User(String name, String age) {
+    public User(String name, String age, String phoneNumber) {
         this.name = name;
         this.age = age;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getName() {
